d9ac21e66f2cf4e0b8b0b83523ab7ffce7877094
Adriana Elizabeth <adrianaelizabeth24@users.noreply.github.com>
2017-06-18 17:54:27 -0500

    Documentation
    comments to code and user manual added

--- calculate.js
+++ calculate.js
@@ -3,26 +3,37 @@
  */
 
 
+/*fucntion that gets called after a number key is pressed
+* param : numberID indicated the id of the key pressed, if the key button is 5, then id will be 5*/
 function clickNumber(numberID){
+    //gets text of aritmetic expressions
     var acum = document.getElementById('aritmeticExpression').getElementsByTagName('div')[0].innerHTML;
     if(acum == "0") {
+        //if aritmetic expression is equal to zero then number pressed display text is changed to number pressed
         document.getElementById('aritmeticExpression').getElementsByTagName('div')[0].innerHTML = numberID;
-        acum = numberID;
     }
     else{
+        //if an aritmetic expression already exists then a new string containing the exixsting expresion concatenated
+        //with the number pressed replaces the text
         var subs = acum.concat(numberID);
         document.getElementById('aritmeticExpression').getElementsByTagName('div')[0].innerHTML = subs;
-        acum = subs
     }
 }
-
+/*function that gets called if +,*,- or / are pressed
+* param : operandID
+* operandID indicates the id of the operand pressed, i.e if * is pressed then id will be * */
 function clickOperand(operandID){
+    //acum gets the value of the aritmetic expression
     var acum = document.getElementById('aritmeticExpression').getElementsByTagName('div')[0].innerHTML;
+    //temp gets the value of the temporary result
     var temp = document.getElementById('screen').getElementsByTagName('div')[0].innerHTML;
+    //if no previous opperation exists then, concatenates the operand to the expression
     if(temp == '0'){
         var subs = acum.concat(operandID);
         document.getElementById('aritmeticExpression').getElementsByTagName('div')[0].innerHTML = subs;
     }
+    //if a previous operation already exits, takes the value of the result, concatenates the opperand
+    //replaces the existing expression with new one
     else{
         var subs = temp.concat(operandID);
         document.getElementById('aritmeticExpression').getElementsByTagName('div')[0].innerHTML = subs;
@@ -30,14 +41,19 @@ function clickOperand(operandID){
     }
 }
 
+/*function that erases value from screen*/
 function clickCE(){
+    //changes values of display to zero
     document.getElementById('aritmeticExpression').getElementsByTagName('div')[0].innerHTML = "0";
     document.getElementById('screen').getElementsByTagName('div')[0].innerHTML = "0";
 }
 
+/* function that works as backspace*/
 function clickBackSpace(){
+    //takes actual expression
     var acum = document.getElementById('aritmeticExpression').getElementsByTagName('div')[0].innerHTML;
     var len = acum.length;
+    //eliminates the last number
     if (len > 1){
         document.getElementById('aritmeticExpression').getElementsByTagName('div')[0].innerHTML = acum.slice(0, -1);
     }
@@ -46,15 +62,20 @@ function clickBackSpace(){
     }
 }
 
+/*function that solves the operation*/
 function clickEquals(){
+    //takes value of the expression
     var acum = document.getElementById('aritmeticExpression').getElementsByTagName('div')[0].innerHTML;
+    //conection with server side
     var xhttp;
     xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
+            //will get the answer of the operation
             document.getElementById('screen').getElementsByTagName('div')[0].innerHTML = this.responseText;
         }
     };
+    //calls the function in server
     xhttp.open("GET", "solve.php?q="+acum, true);
     xhttp.send();
 
--- solve.php
+++ solve.php
@@ -6,18 +6,29 @@
  * Time: 05:54 PM
  */
 
+//variable that represents the expression as string
 $q = $_REQUEST["q"];
+//solves the * and / of string
 $string = replace($q, '*', '/');
+//solves the + and - of string
 $string2 = replace($string, 't', '-');
 
+/*funcition that solves an aritmetic operation
+params: string --> aritmetic expression
+op1 and op2 some aritmetic expression such * and / that has same order of priority
+*/
 function replace($string, $op1, $op2)
 {
     $valor = "";
     $posicionInicial = "";
     $posicionFinal = "";
+    //position of first operand
     $pos1 = strpos($string, $op1);
+    //position of second operand
     $pos2 = strpos($string, $op2);
+    //checks if any operations with that aritmetic sign exists
     while (($pos1 != false) || ($pos2 != false)) {
+        //gets the position of the operation that comes first
         if ($pos1 == false) {
             $pos = $pos2;
             $op = $op2;
@@ -35,6 +46,7 @@ function replace($string, $op1, $op2)
 
         }
         $num2 = "";
+        //second number of the operation
         for ($i = $pos + 1; $i < strlen($string); $i++) {
             if (($string[$i] != 't') && ($string[$i] != '-') && ($string[$i] != '*') && ($string[$i] != '/')) {
                 $num2 .= $string[$i];
@@ -44,6 +56,7 @@ function replace($string, $op1, $op2)
             }
         }
         $numAux = "";
+        //first numeber of the operation
         for ($i = $pos - 1; $i >= 0; $i--) {
             if (($string[$i] != 't') && ($string[$i] != '-') && ($string[$i] != '*') && ($string[$i] != '/')) {
                 $numAux .= $string[$i];
@@ -53,6 +66,7 @@ function replace($string, $op1, $op2)
             }
         }
         $num1 = strrev($numAux);
+        //makes aritmetic operation and stores result in variable $valor
         if ($op == 't') {
             $valor = sum($num1, $num2);
         } elseif ($op == '-') {
@@ -66,19 +80,26 @@ function replace($string, $op1, $op2)
                 echo("Error");
             }
         }
+        //replaces the part number1 operation number2 by the new result of the operation
         $substring = substr($string, $posicionInicial, ($posicionFinal - $posicionInicial) + 1);
         $string = str_replace($substring, $valor, $string);
+        //to continue loop
         $pos1 = strpos($string, $op1);
         $pos2 = strpos($string, $op2);
     }
     return $string;
 }
 
+/*function that sums two values
+params: a, b are the ones that are to be sum
+used to prevent confusion between sum and concatenation of values
+*/
 function sum($a, $b)
 {
     $c = $a + $b;
     return $c;
 }
 
+//returns the answer to server side
 echo($string2);
 ?>
